fbf58e3a76bc170a304e46eeefa732afbab610f7
Daniel Scherf <git@heap.mozmail.com>
2025-05-20 12:32:26 +0200

    Fix setup message

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -10,12 +10,14 @@ use poise::serenity_prelude::{CreateActionRow, CreateButton, CreateEmbed, Create
 pub async fn setup(ctx: Context<'_>, #[description = "The channel to use"] channel: GuildChannel) -> Result<(), Error> {
     let _ = ctx.defer_ephemeral().await;
 
+    let client_id = ctx.data().client_id;
+    let auth_url = format!("https://anilist.co/api/v2/oauth/authorize?client_id={client_id}&redirect_uri=https://anilist.co/api/v2/oauth/pin&response_type=code");
     let _ = channel.send_message(ctx.http(), CreateMessage::new()
         .embed(CreateEmbed::new()
             .title("**Verify your account**")
-            .description("To combat spam, we require you to link your AniList account with your Discord account.\n
+            .description(format!("To combat spam, we require you to link your AniList account with your Discord account.\n
             \
-            Please **get an authorization token from [here](https://anilist.co/api/v2/oauth/authorize?client_id=26866&redirect_uri=https://anilist.co/api/v2/oauth/pin&response_type=code)** and then **click the button below** to initiate the verification process.")
+            Please **get an authorization token from [here]({auth_url})** and then **click the button below** to initiate the verification process."))
             .color(message::primary_color()))
         .components(vec![CreateActionRow::Buttons(vec![
             CreateButton::new("verify_button")

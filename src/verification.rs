9d521274b1f969bf8f890d94baafaf0f00bb13b4
Daniel Scherf <git@heap.mozmail.com>
2025-05-20 12:33:31 +0200

    Refactor verification code

--- src/verification.rs
+++ src/verification.rs
@@ -11,13 +11,8 @@ pub async fn verify(user_id: u64, code: String, data: &Data) -> Result<()> {
     }
     
     let self_user = self_user_result?;
-    let created_at = self_user.created_at.unwrap_or(0);
     let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
-    let account_age = Duration::from_secs(now - created_at as u64);
-    if account_age < data.minimum_account_age {
-        let remaining_time_hours = (data.minimum_account_age.abs_diff(account_age).as_secs() as f64 / 60.0 / 60.0).ceil() as u32;
-        return Err(Error::new(AccountLinkError::AccountAge {remaining_time_hours}))
-    }
+    check_account_age(data, self_user.created_at.unwrap_or(0) as u64, now).await?;
     
     let link_result = database::link(LinkedAccount {
         discord_id: user_id,
@@ -38,3 +33,14 @@ pub async fn verify(user_id: u64, code: String, data: &Data) -> Result<()> {
     
     Ok(())
 }
+
+async fn check_account_age(data: &Data, created_at: u64, now: u64) -> Result<()> {
+    let account_age = Duration::from_secs(now - created_at);
+    if account_age >= data.minimum_account_age {
+        return Ok(())
+    }
+
+    let remaining_time_hours = (data.minimum_account_age.abs_diff(account_age).as_secs() as f64 / 60.0 / 60.0).ceil() as u32;
+    Err(Error::new(AccountLinkError::AccountAge {remaining_time_hours}))
+}
+
